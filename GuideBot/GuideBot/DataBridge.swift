4213133c8229395ecb91c2ab667ad76ffe29de43
jagaa <jagaa1103@gmail.com>
2017-04-13 12:20:36 +0200

    added cocoapods FirebaseDatabse, FirebaseStorage and FirebaseAuth

---
+++ GuideBot/GuideBot/DataBridge.swift
@@ -0,0 +1,18 @@
+//
+//  DataBridge.swift
+//  GuideBot
+//
+//  Created by Enkhjargal Gansukh on 13/04/2017.
+//  Copyright © 2017 Enkhjargal Gansukh. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class DataBridge: NSObject {
+    static let instance = DataBridge()
+    
+    override init() {
+        super.init()
+    }
+}
